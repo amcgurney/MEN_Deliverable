5567b59cc2610c975220ce8d38f688c14877ec10
Alan McGurney <mcgurneyalan@gmail.com>
2022-01-21 12:09:46 -0500

    html and server working

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const methodOverride = require('method-override');
 
 const PORT = 4000;
 
+//set app
+app.set('view engine', 'ejs');
+
 //app.use for adding
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(methodOverride('_method'))
 
 // routes
+
+app.get("/new", function(req, res) {
+    res.render("new.ejs")
+})
+
 app.listen(PORT, function() {
     console.log(`I am listening on port ${PORT}`)
 });
