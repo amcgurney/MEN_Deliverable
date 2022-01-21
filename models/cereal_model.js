fcb7f0f441f72c18c569c0d1834289e45598ce0c
Alan McGurney <mcgurneyalan@gmail.com>
2022-01-21 13:54:27 -0500

    schema model update

---
+++ models/cereal_model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const cerealSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: [true, 'name can not be empty']
+    },
+    price: {
+        type: Number,
+        min: [0, 'you can not add a negative number'],
+        required: [true, 'price can not be empty'],
+    },
+    image: {
+        type: String,
+        required: [true, "image can not be empty"],
+    },
+    description: {
+        type: String,
+    }
+});
+
+const Cereal = mongoose.model('Cereal', cerealSchema);
+module.exports = Cereal;
